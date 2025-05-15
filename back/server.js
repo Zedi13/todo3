ae13c1cdca0be677961935d442f8b1bea5c28516
Zedi13 <chatainflorian1@gmail.com>
2025-05-15 16:00:51 +0200

    json

--- back/server.js
+++ back/server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
+;
 
 const FIREBASE_URL = 'https://reactexo3-default-rtdb.europe-west1.firebasedatabase.app/'; // Remplace <TON_PROJECT_ID>
 
